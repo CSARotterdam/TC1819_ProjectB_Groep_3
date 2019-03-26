c3b11ba49d9e38adf4b663c92be67a98b7f58e69
EduardTer <45460962+EduardTer@users.noreply.github.com>
2019-03-26 12:24:50 +0100

    application setup
    Nieuw android project

---
+++ app/app/src/main/java/groep3/hr/nl/techlabhr/MainActivity.java
@@ -0,0 +1,13 @@
+package groep3.hr.nl.techlabhr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
