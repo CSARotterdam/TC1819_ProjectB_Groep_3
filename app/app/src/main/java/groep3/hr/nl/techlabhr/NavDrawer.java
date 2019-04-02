29e62de6c57e6d6a794cb830de6669842fdc26e6
EduardTer <45460962+EduardTer@users.noreply.github.com>
2019-04-02 16:48:58 +0200

    inventaris als startpunt gezet

--- app/app/src/main/java/groep3/hr/nl/techlabhr/NavDrawer.java
+++ app/app/src/main/java/groep3/hr/nl/techlabhr/NavDrawer.java
@@ -1,5 +1,6 @@
 package groep3.hr.nl.techlabhr;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
@@ -47,6 +48,8 @@ public class NavDrawer extends AppCompatActivity {
                         return true;
                     }
                 });
+
+        selectDrawerItem(nvDrawer.getMenu().findItem(R.id.nav_inventaris));
     }
 
     public void configureDrawer(NavigationView nv){
