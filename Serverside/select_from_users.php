1275a685fee5005dc4c226447154848eec70e304
GLHolmstroff <q.verheij@hotmail.com>
2019-05-08 10:25:37 +0200

    Update select_from_users.php
    Removed deprecated require statement

--- Serverside/select_from_users.php
+++ Serverside/select_from_users.php
@@ -9,9 +9,6 @@ $response["Success"] = 0;
 if (isset($_POST["Email"]) && isset($_POST["Password"])){
 	$Email = $_POST["Email"];
 	$Pass = $_POST["Password"];
-	
-	// include db connect class
-	require_once __DIR__ . '/db_connect.php';
 	 
 	// connecting to db
 	$db = new PDO('sqlite:Test.db');
