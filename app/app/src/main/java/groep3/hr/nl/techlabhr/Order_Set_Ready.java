a4b588ca29d474f229e1aa8ef8ee12d680a12fcc
Quinten Verheij <Q.verheij@hotmail.com>
2019-05-27 17:23:04 +0200

    Functionality for loading all orders into a list and summing Amount of products
    Dont you dare look at the code for summing those objects.
    It's so ugly it'll make your eyes bleed

---
+++ app/app/src/main/java/groep3/hr/nl/techlabhr/Order_Set_Ready.java
@@ -0,0 +1,270 @@
+package groep3.hr.nl.techlabhr;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Order_Set_Ready extends Fragment {
+
+
+    private ListView lv;
+    ArrayList<HashMap<String,String>> orderList;
+
+    private Order_Set_Ready.OnFragmentInteractionListener mListener;
+    private ProgressDialog pDialog;
+
+    // json object response url
+    private String urlJsonObj = "https://eduardterlouw.com/techlab/get_all_pending_orders.php";
+    private String TAG = NavDrawer.class.getSimpleName();
+
+    private static final String TAG_ORDERID = "OrderID";
+    private static final String TAG_EMAIL = "Email";
+    private static final String TAG_AMOUNT_TOTAL = "Total amount of items";
+
+
+    public Order_Set_Ready() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment Order_Set_Ready.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Order_Set_Ready newInstance() {
+        Order_Set_Ready fragment = new Order_Set_Ready();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        //// Set title and menu to appropriate fragment
+        Toolbar toolbar= (Toolbar) getActivity().findViewById(R.id.toolbar);
+        NavigationView nav = (NavigationView) getActivity().findViewById(R.id.nav_view);
+        MenuItem menuItem = (MenuItem) nav.getMenu().findItem(R.id.nav_change_stock);
+        menuItem.setChecked(true);
+        toolbar.setTitle("Product Wijzigen");
+        //Initialize pdialog
+        pDialog = new ProgressDialog(getActivity());
+        pDialog.setMessage("Please wait...");
+        pDialog.setCancelable(false);
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_order_ready, container, false);
+        // Load all Orders into list
+        lv = (ListView) view.findViewById(R.id.listResponse);
+        getAllOrders();
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Order_Set_Ready_Single fragment =(Order_Set_Ready_Single) Order_Set_Ready_Single.newInstance();
+                Bundle product = new Bundle();
+                product.putString(TAG_ORDERID,((TextView) view.findViewById(R.id.pid)).getText().toString());
+                fragment.setArguments(product);
+                Log.d(TAG,product.toString());
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container,fragment).addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+
+        return view;
+    }
+    public void getAllOrders() {
+        showpDialog();
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                urlJsonObj, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+                orderList = new ArrayList<HashMap<String, String>>();
+                try {
+                    int OrderID = 0;
+                    String Email = "";
+                    if(response.getInt("Success")==1) {
+                        JSONArray Orders = (JSONArray) response.get("Orders");
+                        int OrderIDPrev = 0;
+                        String EmailPrev = "";
+                        int ProductAmountTotal = 0;
+
+                        //Code to execute for each order item
+                        for (int i = 0; i < Orders.length(); i++) {
+
+                            JSONObject order = (JSONObject) Orders.get(i);
+                            // Parsing json object response
+                            // response will be a json object
+                            OrderID = order.getInt("OrderID");
+                            Email = order.getString("Email");
+                            int ProductAmount = order.getInt("ProductAmount");
+
+                            //Code that's executed when orderitem from new order is reached
+                            if (OrderID != OrderIDPrev){
+                                //Check if not first item
+                                if (OrderIDPrev != 0){
+                                    //Add previous item in response to orderlist, then overwrite control fields
+                                    HashMap<String,String> map = new HashMap<String,String>();
+                                    map.put(TAG_ORDERID,Integer.toString(OrderIDPrev));
+                                    map.put(TAG_EMAIL,EmailPrev);
+                                    map.put(TAG_AMOUNT_TOTAL,Integer.toString(ProductAmountTotal));
+                                    orderList.add(map);
+
+                                    OrderIDPrev = OrderID;
+                                    EmailPrev = Email;
+                                    ProductAmountTotal = ProductAmount;
+                                }else{
+                                    OrderIDPrev = OrderID;
+                                    EmailPrev = Email;
+                                    ProductAmountTotal = ProductAmount;
+                                }
+                            }else{
+                                OrderIDPrev = OrderID;
+                                EmailPrev = Email;
+                                ProductAmountTotal += ProductAmount;
+                            }
+                        }
+                        //Add final item in response to orderlist
+                        //TODO: I am 99% sure there is a better way to do this, so find out how and implement that instead.
+                        HashMap<String,String> map = new HashMap<String,String>();
+                        map.put(TAG_ORDERID,Integer.toString(OrderID));
+                        map.put(TAG_EMAIL,Email);
+                        map.put(TAG_AMOUNT_TOTAL,Integer.toString(ProductAmountTotal));
+                        orderList.add(map);
+
+                        Log.d(TAG,orderList.toString());
+                        hidepDialog();
+                        getActivity().runOnUiThread(new Runnable() {
+                            public void run() {
+                                /**
+                                 * Updating parsed JSON data into ListView
+                                 * */
+                                ListAdapter adapter = new SimpleAdapter(
+                                        getActivity(), orderList,
+                                        R.layout.order_list_item, new String[] { TAG_ORDERID,
+                                        TAG_EMAIL,TAG_AMOUNT_TOTAL},
+                                        new int[] { R.id.OrderID, R.id.user_email,R.id.product_amount_total });
+
+                                lv.setAdapter(adapter);
+                            }
+                        });
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getActivity().getApplicationContext(),
+                            "JSONError: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+                hidepDialog();
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getActivity().getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+                // hide the progress dialog
+                hidepDialog();
+            }
+        });
+
+        // Adding request to request queue
+        SingletonQueue.getInstance().addToRequestQueue(jsonObjReq);
+
+    }
+
+    private void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/app/src/main/java/groep3/hr/nl/techlabhr/Order_Set_Ready_Single.java
@@ -0,0 +1,200 @@
+package groep3.hr.nl.techlabhr;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Order_Set_Ready_Single extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters\
+
+    private String selectURL = "https://eduardterlouw.com/techlab/select_from_products.php";
+    private String TAG = NavDrawer.class.getSimpleName();
+    private static final String TAG_SUCCESS = "Success";
+    private static final String TAG_PRODUCTS = "Products";
+    private static final String TAG_PID = "ProductID";
+    private static final String TAG_MANUFACTURER = "ProductManufacturer";
+    private static final String TAG_CATEGORY = "ProductCategory";
+    private static final String TAG_NAME = "ProductName";
+    private static final String TAG_AMOUNT = "Amount";
+    private static final String TAG_STOCK = "ProductStock";
+
+
+    private ProgressDialog pDialog;
+    private ListView lv;
+
+    private Button btnSetOrderReady;
+
+    private Order_Set_Ready_Single.OnFragmentInteractionListener mListener;
+
+    public Order_Set_Ready_Single() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     *
+     * @return A new instance of fragment Placeholder.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Order_Set_Ready_Single newInstance() {
+        Order_Set_Ready_Single fragment = new Order_Set_Ready_Single();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Set title and menu to appropriate fragment
+        Toolbar toolbar= (Toolbar) getActivity().findViewById(R.id.toolbar);
+        NavigationView nav = (NavigationView) getActivity().findViewById(R.id.nav_view);
+        MenuItem menuItem = (MenuItem) nav.getMenu().findItem(R.id.nav_inventaris);
+        menuItem.setChecked(true);
+        toolbar.setTitle("Inventaris");
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_order_ready_single, container, false);
+
+        lv = (ListView) view.findViewById(R.id.listResponse);
+
+
+        btnSetOrderReady.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // Adding product to cart
+                setOrderReady();
+
+            }
+
+        });
+
+        pDialog = new ProgressDialog(getActivity());
+        pDialog.setMessage("Please wait...");
+        pDialog.setCancelable(false);
+        readSingleProduct();
+        return view;
+    }
+
+    private void setOrderReady() {
+        //TODO: Make request to php file setting all items with corresponding OrderID to ready for pickup
+    }
+
+    /*
+     * JsonObjectRequest takes in five paramaters
+     * Request Type - This specifies the type of the request eg: GET,POST
+     *
+     * URL - This String param specifies the Request URL
+     *
+     * JSONObject - This parameter takes in the POST parameters."null" in
+     * case of GET request.
+     *
+     * Listener -This parameter takes in a implementation of Response.Listener()
+     * interface which is invoked if the request is successful
+     *
+     * Listener -This parameter takes in a implementation of Error.Listener()
+     * interface which is invoked if any error is encountered while processing
+     * the request
+     */
+    private void readSingleProduct() {
+    }
+
+    private void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
