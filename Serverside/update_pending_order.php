473ba6904c65e6163f947fc505b7a53d07ed0b43
Quinten Verheij <Q.verheij@hotmail.com>
2019-06-02 16:46:07 +0200

    Setting order as ready now removes items from active stock
    Also added alternate php file for denying orders

--- Serverside/update_pending_order.php
+++ Serverside/update_pending_order.php
@@ -14,12 +14,20 @@ if (isset($_POST["OrderID"])){
 
     // connecting to db
     $db = new PDO('sqlite:Test.db');
-
+    $products = array();
 
     $sql =<<<EOF
       UPDATE Orders
 	  SET 	Status = "readyForPickup"
 	  WHERE OrderID = :ID
+EOF;
+    $sql2 =<<<EOF
+      SELECT ProductID,ProductAmount from Orders WHERE OrderID = :ID
+EOF;
+    $sql3 =<<<EOF
+      UPDATE Products
+	  SET 	ProductAmountInProgress = ProductAmountInProgress + :Amount
+	  WHERE ProductID = :ID
 EOF;
     $stmt = $db->prepare($sql);
     $stmt->bindParam(':ID',$ID);
@@ -29,6 +37,23 @@ EOF;
         $response = $db->lastErrorMsg();
     }else{
         $response = "Order successfully updated";
+    }
+
+    $stmt = $db->prepare($sql2);
+    $stmt->execute([':ID'=>$ID]);
+    //construct array for updating inventory
+    while($row = $stmt->fetch(\PDO::FETCH_ASSOC) ) {
+        $product = array();
+        $product['ProductID'] = $row['ProductID'];
+        $product['ProductAmount'] = $row['ProductAmount'];
+        array_push($products,$product);
+    }
+    //update product database
+    foreach($products as $arrItem){
+        $stmt = $db->prepare($sql3);
+        $stmt->bindParam(':Amount',$arrItem["ProductAmount"]);
+        $stmt->bindParam(':ID',$arrItem["ProductID"]);
+        $stmt->execute();
     }
 }else{
 
