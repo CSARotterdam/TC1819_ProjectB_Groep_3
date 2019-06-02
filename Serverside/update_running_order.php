cd3aff557184b8e26733755cea66f8a140142630
Quinten Verheij <Q.verheij@hotmail.com>
2019-06-02 16:55:18 +0200

    Completed realtime active stock display
    Marking orders as handed in now puts the correct amounts back into the active stock

--- Serverside/update_running_order.php
+++ Serverside/update_running_order.php
@@ -14,12 +14,23 @@ if (isset($_POST["OrderID"])){
 
     // connecting to db
     $db = new PDO('sqlite:Test.db');
-
+    $products = array();
 
     $sql =<<<EOF
       UPDATE Orders
 	  SET 	Status = "Completed"
 	  WHERE OrderID = :ID
+EOF;
+    $sql2 =<<<EOF
+      SELECT ProductID,ProductAmount from Orders WHERE OrderID = :ID
+EOF;
+    $sql3 =<<<EOF
+      UPDATE Products
+	  SET 	ProductAmountInProgress = CASE
+	                                        WHEN ProductAmountInProgress - :Amount < 0 THEN 0
+	                                        ELSE ProductAmountInProgress - :Amount
+	                                        END 
+	  WHERE ProductID = :ID
 EOF;
     $stmt = $db->prepare($sql);
     $stmt->bindParam(':ID',$ID);
@@ -29,6 +40,24 @@ EOF;
         $response = $db->lastErrorMsg();
     }else{
         $response = "Order successfully updated";
+    }
+
+    $stmt = $db->prepare($sql2);
+    $stmt->execute([':ID'=>$ID]);
+    //construct array for updating inventory
+    while($row = $stmt->fetch(\PDO::FETCH_ASSOC) ) {
+        $product = array();
+        $product['ProductID'] = $row['ProductID'];
+        $product['ProductAmount'] = $row['ProductAmount'];
+        array_push($products,$product);
+    }
+
+    //update product database
+    foreach($products as $arrItem){
+        $stmt = $db->prepare($sql3);
+        $stmt->bindParam(':Amount',$arrItem["ProductAmount"]);
+        $stmt->bindParam(':ID',$arrItem["ProductID"]);
+        $stmt->execute();
     }
 }else{
 
